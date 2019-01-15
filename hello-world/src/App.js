c10a8d83716561ac0c904e8af380531a82bee17c
tolga <h.tolgaevcimen@gmail.com>
2019-01-15 16:11:38 +0300

    functional component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, react world!</h1>
         <p>This is a paragraph</p>
+        <Person />
       </div>
     );
   }
---
+++ hello-world/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <div>I'm a person</div>
+}
+
+export default person;
