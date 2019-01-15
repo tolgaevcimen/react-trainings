57d8acf8de1ed950f924ab4272a0f4977926c1a5
tolga <h.tolgaevcimen@gmail.com>
2019-01-15 15:52:09 +0300

    İnitial commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, react world!</h1>
+        <p>This is a paragraph</p>
+      </div>
+    );
+  }
+}
+
+export default App;
