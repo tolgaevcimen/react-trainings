35a75497f40a57558bfc48d7dc8b74c9bd021237
tolga <h.tolgaevcimen@gmail.com>
2019-01-15 16:41:07 +0300

    Usage of state variable

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    people: [
+      { name: 'Jackie', age: 12 },
+      { name: 'Jane', age: 33 },
+      { name: 'Jethro', age: 31 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, react world!</h1>
         <p>This is a paragraph</p>
-        <Person name="Jackie" age="12"></Person>
-        <Person name="Jane" age="33">My hobbies are blah blah.</Person>
-        <Person name="Jethro" age="31"></Person>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
+        <Person name={this.state.people[1].name} age={this.state.people[1].age} />
+        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
       </div>
     );
   }
